dc8da0a2f78e6162e610e65ae0906471a6aa5bac
Morrow245 <94848219+Morrow245@users.noreply.github.com>
2021-11-24 20:04:41 +0300

    Add files via upload

---
+++ new.py
@@ -0,0 +1,13 @@
+# name = input('Введите ваше имя')
+# print('Привет' + name)
+# age = int(input('Введите возраст'))
+# print(type(age))
+
+# name = input('Введите свое имя:')
+# print('Привет, %s' % name)
+# print('Hello', {}!.format(name) )
+
+i = 0
+while i < 3:
+    print('Привет Саша')
+    i = i + 1
