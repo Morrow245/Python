9bf526e4f1391c0ca16fb65b1b66da2f2b091952
Morrow245 <94848219+Morrow245@users.noreply.github.com>
2021-12-12 21:22:01 +0300

    Add files via upload

--- work_7.py
+++ work_7.py
@@ -1,14 +1,27 @@
-from itertools import count
-from math import factorial
-def fgen():
-    for el in count(1):
-        yield factorial(el)
-
-gen = fgen()
-n = 0
-for elem in gen:
-    if n < 6:
-        print(elem)
-        n += 1
+import json
+profit = {}
+pr = {}
+prof = 0
+prof_aver = 0
+i = 0
+with open('f7.txt', 'r') as file:
+    for line in file:
+        name, form_ownership, earning, damage = line.split()
+        profit[name] = int(earning) - int(damage)
+        if profit.setdefault(name) >= 0:
+            prof = prof + profit.setdefault(name)
+            i += 1
+    if i != 0:
+        prof_aver = prof / i
+        print(f'Прибыль средняя - {prof_aver:.2f}')
     else:
-        break
+        print(f'Компании работают в убыток')
+    pr = {'average profit': round(prof_aver)}
+    profit.update(pr)
+    print(f'Прибыль компаний - {profit}')
+
+with open('f7.json', 'w') as write_js:
+    json.dump(profit, write_js)
+    js_str = json.dumps(profit)
+    print(f'Создан файл с расширением json с содержимым: \n'
+          f'{js_str}')
